baa3e289d4e0909318e3f6f4136c18b66a476766
ruuka-huang <95245931+ruuka-huang@users.noreply.github.com>
2022-03-14 02:07:54 -0400

    Publications page

--- pages/clinical-bioinformatics.vue
+++ pages/clinical-bioinformatics.vue
@@ -1,10 +1,39 @@
 <template>
   <div class="flex w-full">
     <div class="w-full h-full overflow-y-auto px-2 py-4">
-
-      <div class="flex flex-col w-full">
+    <Breadcrumb title="breadcrumb"/>
+      <!-- <div class="flex flex-col w-full">
         <div class="divider">Clinical Bioinformatics</div>
-      </div>
+      </div> -->
+
+    <div class="bg-white shadow-sm w-full rounded mt-2">
+        <h4 class="text-xl lg:text-2xl px-3 py-2 leading-tight text-black">New Publications</h4>
+    </div>
+
+
+       <!-- START CAROUSEL -->
+       <div class="flex flex-wrap flex-col lg:flex-row mt-4">
+          <div class="flex-1  bg-transparent h-[350px]">
+             <Carousel v-if="isSplideLoaded"/>
+          </div>
+
+        <div class="w-full lg:w-1/3 px-1">
+               <WhatsTrending />
+        </div>
+       </div>
+
+       <!-- END CAROUSEL -->
+
+       <!-- START PUBLICATIONS SECTION -->
+  <section class="my-10 ">
+      <!-- START CARD COMPONENT  -->
+<publicationsCard v-for="(publication, index) in publications" :key="index" :publication="publication" />
+      <!-- END CARD COMPONENT -->
+  </section>
+
+  <!-- END PUBLICATIONS SECTION -->
+
+
     </div>
   </div>
 
@@ -12,29 +41,104 @@
 
 
 <script>
+import  Carousel from '../components/publication_components/Carousel.vue'
+import  WhatsTrending from '../components/publication_components/WhatsTrending.vue'
+import  publicationsCard from '../components/publication_components/publicationsCard.vue'
+import  Breadcrumb from '../components/publication_components/Breadcrumb.vue'
+
+
+
+
 export default {
   name: 'ClinicalBioinformatics',
+  components: {
+    Carousel,
+    WhatsTrending,
+    publicationsCard,
+    Breadcrumb
+
+  },
   data() {
     return {
       pageTitle: 'Bioinformatics Data Hub - Clinical Bioinformatics',
       pageSubTitle: "A portal to access and analyze Gilead's molecular data.",
-    }
+        isSplideLoaded: false,
+        publications: [
+          {
+            title:"Methylation signatures in peripheral blood are associated with marked age acceleration and disease progression in patients with primary sclerosing cholangitis",
+            status:"live",
+            starts_in:"starts in 2 days and 21 hours",
+            url:null,
+            image:require('../static/photo.jpg'),
+            description:null,
+            authors:"Authors: Trauner M, Gindin Y, Jiang Z, Chung C, Subramanian GM, Myers RP, Gulamhusein A, Kowdley KV, Levy C, Goodman Z, Manns MP",
+            date:null,
+            ends_in:null,
+            year:"2020",
+            internal_lead:"Yevgeniy Gindin",
+            journal:"Journal of Hepatology Reports",
+            category:null,
+            top_read:null,
+          },
+          {
+            title:"Longitudinal analysis of serum microRNAs as predictors of cirrhosis regression during treatment of hepatitis B virus infection",
+            status:"ended",
+            starts_in:null,
+            url:null,
+            image:"http://placehold.jp/378x200.png",
+            description:null,
+            authors:"Orr C, Myers R, Li B, Jiang Z, Flaherty J, Gaggar A, Meissner EG",
+            date:null,
+            ends_in:"2 days ago",
+            year:"2020",
+            internal_lead:null,
+             journal:null,
+            category:"Clinical",
+             top_read:null,
 
-    var y0 = [];
-    var y1 = [];
-    for (var i = 0; i < 50; i ++) {
-      y0[i] = Math.random();
-      y1[i] = Math.random() + 1;
-    }
 
+          },
+                    {
+            title:"New Seminar Lecture: Bioinformatics",
+            status:"ended",
+            starts_in:null,
+            url:null,
+            image:"http://placehold.jp/378x200.png",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            authors:null,
+            date:null,
+            ends_in:"Yesterday",
+            journal:null,
+            year:null,
+            internal_lead:null,
+            category:"Clinical",
+             top_read:true,
+
+
+          }
+        ],
+    }
   },
 
   head() {
     const title = 'Public Data Sets'
     return {
       title,
+        script: [
+        {
+          // FAQ
+          hid: 'splide',
+          src: 'https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/js/splide.min.js',
+          defer: false,
+           callback: () => { this.isSplideLoaded = true }
+        },
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css' }
+      ]
     }
   },
 }
 </script>
+
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,109 +1,9 @@
 <template>
   <div class="flex w-full">
-<!--    <div class="flex flex-col w-full px-2 py-2 overflow-y-auto border-b lg:border-r lg:h-screen lg:w-80">-->
 
-<!--      <div class="flex flex-col justify-between mt-6">-->
-<!--        <aside>-->
-<!--          <ul>-->
-<!--            <li>-->
-<!--              <a class="flex items-center px-4 py-2 text-primary bg-gray-100 rounded-md " href="#">-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"-->
-<!--                     stroke="currentColor">-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />-->
-<!--                </svg>-->
-
-<!--                <NuxtLink to="/">-->
-<!--                  <span class="mx-4 font-medium">Home</span>-->
-<!--                </NuxtLink>-->
-<!--              </a>-->
-<!--            </li>-->
-
-<!--            <li>-->
-<!--              <a class="flex items-center px-4 py-2 mt-5 text-gray-600 rounded-md hover:bg-gray-200" href="#">-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"-->
-<!--                     stroke="currentColor">-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />-->
-<!--                </svg>-->
-
-<!--                <NuxtLink to="/CustomPlot">-->
-<!--                  <span class="mx-4 font-medium">Clinical Trial Data</span>-->
-<!--                </NuxtLink>-->
-<!--              </a>-->
-<!--            </li>-->
-
-<!--            <li>-->
-<!--              <a class="flex items-center px-4 py-2 mt-5 text-gray-600 rounded-md hover:bg-gray-200" href="#">-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"-->
-<!--                     stroke="currentColor">-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />-->
-<!--                </svg>-->
-
-<!--                <span class="mx-4 font-medium">Public Datasets</span>-->
-<!--              </a>-->
-<!--            </li>-->
-
-<!--            <li>-->
-<!--              <a class="flex items-center px-4 py-2 mt-5 text-gray-600 rounded-md hover:bg-gray-200" href="#">-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"-->
-<!--                     stroke="currentColor">-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />-->
-<!--                </svg>-->
-
-<!--                <span class="mx-4 font-medium">Clinical Bioinformatics</span>-->
-<!--              </a>-->
-<!--            </li>-->
-<!--            <li>-->
-<!--              <a class="flex items-center px-4 py-2 mt-5 text-gray-600 rounded-md hover:bg-gray-200" href="#">-->
-<!--                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"-->
-<!--                     stroke="currentColor">-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />-->
-<!--                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />-->
-<!--                </svg>-->
-
-<!--                <span class="mx-4 font-medium">Other Tools-->
-<!--                  <font-awesome-icon :icon="['far', 'angle-down']"  />-->
-<!--                </span>-->
-<!--              </a>-->
-<!--            </li>-->
-<!--          </ul>-->
-
-<!--        </aside>-->
-
-<!--      </div>-->
-<!--    </div>-->
     <div class="w-full h-full overflow-y-auto p-10">
 
       <img src="~static/datahub-home.png">
-
-<!--      <div class="hero min-h-screen" style="background-image: url('');">-->
-<!--        <div class="backdrop-blur-sm bg-white/30"></div>-->
-<!--        <div class="text-center hero-content text-neutral-content">-->
-<!--          <div class="max-w-md">-->
-<!--            <h1 class="mb-5 text-5xl font-light">-->
-<!--          <span>-->
-<!--            <span class="text-gilead-red">Welcome, </span>-->
-<!--            <span class="span1 text-red-600">John<br /></span>-->
-<!--            <span class="span2 text-red-300">to Bioinformatics Data Hub</span>-->
-<!--          </span>-->
-<!--            </h1>-->
-<!--            <p class="mb-5 text-red-300">-->
-<!--              This website serves as a data hub for molecular...-->
-<!--            </p>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
 
     </div>
   </div>
@@ -127,9 +27,9 @@ export default {
       script: [
         {
           // FAQ
-          // id: 'elfsight',
-          // src: 'https://apps.elfsight.com/p/platform.js',
-          // defer: true,
+          id: 'splide',
+          src: 'https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/js/splide.min.js',
+          defer: true,
         },
       ],
     }
