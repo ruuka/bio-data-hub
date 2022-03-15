2a66053b44a59a22f059c1a10e26e6ebb04ee1cd
ManishKarki1997 <manishkarki247@gmail.com>
2022-03-15 21:29:44 +0545

    make searchable select filter work

--- components/layout_components/selectInput.vue
+++ components/layout_components/selectInput.vue
@@ -1,32 +1,58 @@
 <template>
   <div class="flex flex-wrap items-center gap-4">
-    <select
+    <vue-tags-input
       v-for="(subFilter, idx) in filter.axisSubFilters"
       :key="subFilter.label + idx + subFilter.parentId"
-      v-model="subFilter.selectedValue"
-      class="select select-bordered"
-      :placeholder="subFilter.label"
+      v-model="filterSearchTextObj[subFilter.id]"
+      :max-tags="subFilter.isMultipleSelect ? maxLengthForMultipleSelect : 1"
       :disabled="isDisabled(idx)"
-      @change="onSelectChange($event, subFilter)"
+      :tags="getTagsForSubFilter(subFilter)"
+      :autocomplete-items="getAutocompleteItems(subFilter.id)"
+      class="tags-input"
+      :class="{ 'is-filled': isInputFilled(subFilter) }"
+      :placeholder="getPlaceholder(idx)"
+      :autocomplete-always-open="true"
+      :add-only-from-autocomplete="true"
+      @focus="onFocus(subFilter)"
+      @blur="onBlur"
+      @tags-changed="(newTags) => onTagsChanged(newTags, subFilter)"
     >
-      <!-- if you remove value="" from below option, that option won't show up as placeholder for the select -->
-      <!-- i guess depends on how you like it -->
-      <!-- the default placeholder won't work because of v-model in the select tag, try and remove this value="" below and try it yourself :)-->
-      <option value="" disabled selected>- {{ subFilter.label }} -</option>
-      <option
-        v-for="option in subFilter.options"
-        :key="
-          'sub-filter-option-' +
-          option.value +
-          '-for-' +
-          subFilter.id +
-          filter.id
-        "
-        :value="option.value"
+      <!-- leave this slot as empty element, otherwise the library's default close icon will show up -->
+      <!-- we're using our own svg icon below in the slot "tag-center" -->
+      <div slot="tag-actions"></div>
+      <div slot="tag-center" slot-scope="props" class="gap-2 text-xs">
+        {{ props.tag.text }}
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block w-4 h-4 cursor-pointer stroke-current"
+          @click.prevent="props.performDelete(props.index)"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          ></path>
+        </svg>
+      </div>
+
+      <div
+        slot="autocomplete-item"
+        slot-scope="props"
+        class="autocomplete-item"
+        @click.prevent="props.performAdd(props.item)"
       >
-        {{ option.name }}
-      </option>
-    </select>
+        <h6 class="text-xs font-semibold">
+          {{ props.item.text }}
+        </h6>
+        <p class="text-xs text-gray-700">
+          {{ props.item.text }}
+        </p>
+      </div>
+    </vue-tags-input>
   </div>
 </template>
 
@@ -91,19 +117,118 @@ export default {
       ],
 
       // variables for my implementation
+      filterSearchTextObj: {},
+      maxLengthForMultipleSelect: 2,
+      focusedInputId: '',
+      // an object to hold search texts for the different subfilters,
+      // if every vue-tag-input select used same variable, then if you type on one input, it would show up on every input
     }
   },
 
   computed: {},
   mounted() {},
   methods: {
-    onSelectChange(evt, subFilter) {
-      const selectedValue = evt.target.value
+    isInputFilled(subFilter) {
+      if (
+        subFilter.isMultipleSelect === undefined ||
+        !subFilter.isMultipleSelect
+      ) {
+        // for single select, if there's one selected value, its filled
+        return subFilter.selectedValue.length === 1
+      }
 
+      // for multple selected, it is filled if the array length is >= the max allowed
+      return subFilter.selectedValue.length >= this.maxLengthForMultipleSelect
+    },
+    getPlaceholder(idx) {
+      const subFilter = this.filter.axisSubFilters[idx]
+
+      if (subFilter.isMultipleSelect) {
+        if (
+          subFilter.selectedValue.length === this.maxLengthForMultipleSelect
+        ) {
+          return ''
+        }
+      }
+
+      // if the subfilter is single select and it already has a value selected, return empty placeholder
+      if (
+        (subFilter.isMultipleSelect === undefined ||
+          !subFilter.isMultipleSelect) &&
+        subFilter.selectedValue.length === 1
+      ) {
+        return ''
+      }
+
+      return subFilter.label
+    },
+    getTagsForSubFilter(subFilter) {
+      // const isMultipleSelect = subFilter?.isMultipleSelect
+
+      return subFilter.selectedValue.map((opt) => ({
+        ...opt,
+        text: opt.name,
+      }))
+    },
+    onTagsChanged(newTags, filter) {
       this.$emit('ON_SELECT_CHANGE', {
-        value: selectedValue,
-        subFilterId: subFilter.id,
+        value: newTags,
+        subFilterId: filter.id,
       })
+    },
+    onFocus(subFilter) {
+      // this whole focus and blur events just to close the autocomplete dropdown when user loses focus from the input
+
+      this.focusedInputId = subFilter.id
+    },
+    onBlur() {
+      // setting a timeout so that the DOM first removes the ".ti-focus" class from the input
+      // then we do the stuff
+
+      setTimeout(() => {
+        if (!document.querySelector('.vue-tags-input.ti-focus')) {
+          this.focusedInputId = ''
+        }
+      }, 100)
+    },
+    getAutocompleteItems(subFilterId, options = { isInputDisabled: false }) {
+      if (options.isInputDisabled) return []
+      // if the input is disabled, return an empty array so that the autocomplete dropdown won't show up when an item is already selected
+
+      const subFilter = this.filter.axisSubFilters.find(
+        (sub) => sub.id === subFilterId
+      )
+
+      if (!subFilter) return []
+
+      if (
+        (subFilter.isMultipleSelect !== undefined ||
+          subFilter?.isMultipleSelect) &&
+        subFilter.selectedValue.length === this.maxLengthForMultipleSelect
+      ) {
+        return []
+      }
+
+      if (
+        subFilter.isMultipleSelect === undefined ||
+        !subFilter.isMultipleSelect
+      ) {
+        // for single select, if it already has a value, send empty array as the autocomplete items list
+        if (subFilter.selectedValue.length === 1) {
+          return []
+        }
+      }
+
+      if (this.focusedInputId !== subFilterId) return []
+
+      const searchTxt =
+        this.filterSearchTextObj[subFilterId] === undefined
+          ? ''
+          : this.filterSearchTextObj[subFilterId]?.toLowerCase()
+
+      return subFilter.options.filter((s) =>
+        s.name.toLowerCase().includes(searchTxt)
+      )
     },
 
     isDisabled(subFilterIdx) {
@@ -121,14 +246,23 @@ export default {
         return false
       }
 
-      // if the current index - 1 (i.e. previous select dropdown) already has the "selectedValue" populated, then this filter should not be disabled
-      // otherwise, if previous select dropdown is not selected, the following dropdown should be disabled
-      // also, as usual, never disable the first one of any row
-      if (
-        subFilterIdx !== 0 &&
-        !this.filter.axisSubFilters[subFilterIdx - 1]?.selectedValue
-      ) {
-        return true
+      if (subFilterIdx !== 0) {
+        const previousSubFilter = this.filter.axisSubFilters[subFilterIdx - 1]
+        const currentSubFilter = this.filter.axisSubFilters[subFilterIdx]
+
+        // if (previousSubFilter.selectedValue.length === 0) return true
+        if (previousSubFilter.selectedValue.length === 0) {
+          // if the previous dropdown has value not seleccted ^
+
+          if (currentSubFilter.selectedValue.length) {
+            // if the current dropdown does have value selected, it means that the previous dropdown had value selected previously
+            // but removed later, so this current dropdown should not be disabled in this case
+
+            return false
+          }
+
+          return true
+        }
       }
 
       return false
@@ -136,3 +270,17 @@ export default {
   },
 }
 </script>
+
+<style>
+.vue-tags-input.ti-disabled {
+  background-color: #d2d2d2 !important;
+}
+
+/* this is the input field */
+
+/* we're remove it when the select dropdown is filled, otherwise, it leaves the input tag, which looks ugly */
+
+.vue-tags-input.is-filled .ti-input .ti-tags .ti-new-tag-input-wrapper {
+  display: none;
+}
+</style>
--- pages/custom-plot.vue
+++ pages/custom-plot.vue
@@ -271,23 +271,27 @@
               <div class="overflow-hidden shadow sm:rounded-md">
                 <div class="px-2 py-2 bg-white sm:p-6">
                   <div class="flex flex-col w-full">
-                    <selectFilter ref="selectFilter" />
+                    <client-only>
+                      <selectFilter ref="selectFilter" />
+                    </client-only>
 
-                    <div
-                      v-for="(axisFilter, idx) in allAxisFilters"
-                      :key="axisFilter.name"
-                      class="flex flex-row mb-4"
-                    >
-                      <div class="px-2 py-2 mt-2">
-                        <p>{{ axisFilter.name }}</p>
+                    <client-only>
+                      <div
+                        v-for="(axisFilter, idx) in allAxisFilters"
+                        :key="axisFilter.name"
+                        class="flex flex-row mb-4"
+                      >
+                        <div class="px-2 py-2 mt-2">
+                          <p>{{ axisFilter.name }}</p>
+                        </div>
+                        <select-input
+                          :disabled-row-indices="wholeRowDisabledIndices"
+                          :filter-index="idx"
+                          :filter="axisFilter"
+                          @ON_SELECT_CHANGE="handleOnSelectChange"
+                        />
                       </div>
-                      <select-input
-                        :disabled-row-indices="wholeRowDisabledIndices"
-                        :filter-index="idx"
-                        :filter="axisFilter"
-                        @ON_SELECT_CHANGE="handleOnSelectChange"
-                      />
-                    </div>
+                    </client-only>
 
                     <!--                      <div class="divider"></div>-->
                     <div class="flex flex-row">
@@ -687,7 +691,8 @@ export default {
           name: 'Study Type',
           parentId: 'study-id',
           label: 'Select Study Type',
-          selectedValue: '',
+          selectedValue: [],
+          isMultipleSelect: true,
           options: [
             {
               name: 'GS-US-321-0105',
@@ -712,7 +717,7 @@ export default {
           name: 'Timepoints',
           parentId: 'study-id',
           label: 'Select Timepoints',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Baseline',
@@ -733,7 +738,7 @@ export default {
           name: 'Ethnicity',
           parentId: 'study-id',
           label: 'Select Ethnicity',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Swedish',
@@ -758,7 +763,7 @@ export default {
           name: 'Data Type',
           parentId: 'horizontal-axis',
           label: 'Select Data Type',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Gene Expression',
@@ -779,7 +784,7 @@ export default {
           name: 'Blood Cell',
           parentId: 'horizontal-axis',
           label: 'Select Blood Cell',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Red Blood Cell',
@@ -796,7 +801,8 @@ export default {
           name: 'Age Range',
           parentId: 'horizontal-axis',
           label: 'Select Age Range',
-          selectedValue: '',
+          selectedValue: [],
+          isMultipleSelect: true,
           options: [
             {
               name: '1 Years Old',
@@ -829,7 +835,7 @@ export default {
           name: 'Data Type',
           parentId: 'vertical-axis',
           label: 'Select Data Type',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Gene Expression',
@@ -850,7 +856,7 @@ export default {
           name: 'Sex',
           parentId: 'vertical-axis',
           label: 'Select Sex',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Male',
@@ -871,7 +877,7 @@ export default {
           name: 'Income Level',
           parentId: 'vertical-axis',
           label: 'Select Income Level',
-          selectedValue: '',
+          selectedValue: [],
           options: [
             {
               name: 'Low',
@@ -914,9 +920,16 @@ export default {
     allAxisFilters() {
       return this.axisFilters.map((af) => ({
         ...af,
-        axisSubFilters: this.axisFilterOptions.filter(
-          (sub) => sub.parentId === af.id
-        ),
+        axisSubFilters: this.axisFilterOptions
+          .filter((sub) => sub.parentId === af.id)
+          .map((sub) => ({
+            ...sub,
+            text: sub.name, // the vue-tags-input library requires a text field
+            options: sub.options.map((opt) => ({
+              ...opt,
+              text: opt.name, // the vue-tags-input library requires a text field
+            })),
+          })),
       }))
     },
     wholeRowDisabledIndices() {
@@ -929,13 +942,33 @@ export default {
 
       // starting from 1 since for the first row (0), it won't ever be fully disabled
       for (let i = 1; i < filterIds.length; i++) {
-        // by for this, i mean the previous row options
-        const axisFilterOptionsForThisIdx = this.axisFilterOptions.filter(
+        const axisFilterOptionsForPrevIdx = this.axisFilterOptions.filter(
           (sub) => sub.parentId === filterIds[i - 1].id
         )
 
-        // if not all select dropdown has their respective select dropdown value, then, this whole row is disabled
-        if (!axisFilterOptionsForThisIdx.every((opt) => opt.selectedValue)) {
+        // if every dropdown in previous row does not have their value selected,
+        // it means there are two cases
+
+        // i: initially, the previous row were all selected, but later on one select dropdown was deselected, in which case, the row after that must not be fully disabled
+        // ii: the usual case where not every dropdown in previous row were selected, and nothing in the next row is selected, so disable this whole next row
+
+        if (
+          !axisFilterOptionsForPrevIdx.every(
+            (opt) => opt.selectedValue.length > 0
+          )
+        ) {
+          const axisFilterOptionsForCurrentIdx = this.axisFilterOptions.filter(
+            (sub) => sub.parentId === filterIds[i].id
+          )
+
+          if (
+            axisFilterOptionsForCurrentIdx.some(
+              (opt) => opt.selectedValue.length > 0
+            )
+          ) {
+            break
+          }
+
           disabledIndices.push(i)
         }
       }
