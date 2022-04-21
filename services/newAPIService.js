fda73da078a96ef72f0417ce1568673d67429a58
ruuka-huang <95245931+ruuka-huang@users.noreply.github.com>
2022-04-21 02:59:52 -0400

    Added API settings

--- services/newAPIService.js
+++ services/newAPIService.js
@@ -1,10 +1,13 @@
 export default {
   getScatterPlotParameters($axios) {
     // console.log('newAPIService working')
-    return $axios.get('/plot-options/scatter-plot-parameters/', {
-      headers: {
-        // 'dev-secret-key': 'let-me-in',
-      },
-    })
-  }
+    return $axios
+      .get('/plot-options/scatter-plot-parameters/')
+      .catch((error) => {
+        console.log('There was an error: ' + error.response)
+      })
+      // headers: {
+      //   // 'dev-secret-key': 'let-me-in',
+      // },
+  },
 }
