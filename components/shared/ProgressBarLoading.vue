ff183da5e3808512d25eb1e0b806d08f9446a294
boratechlife <boratechlife@gmail.com>
2022-06-24 17:33:37 +0300

    add back progress bar file

---
+++ components/shared/ProgressBarLoading.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="spinner">
+  <div class="bounce1"></div>
+  <div class="bounce2"></div>
+  <div class="bounce3"></div>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.spinner {
+  margin: 10px auto 0;
+  width: 70px;
+  text-align: center;
+}
+
+.spinner > div {
+  width: 12px;
+  height: 12px;
+  background-color: #333;
+  border-radius: 100%;
+  display: inline-block;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+}
+
+.spinner .bounce1 {
+  animation-delay: -0.32s;
+}
+
+.spinner .bounce2 {
+  animation-delay: -0.16s;
+}
+
+@keyframes sk-bouncedelay {
+  0%, 80%, 100% { 
+    transform: scale(0);
+  }
+  
+  40% { 
+    transform: scale(1.0);
+  }
+}
+    
+</style>
