dccbd87ff8208bad4b74f76efcf23ef37dc6b22c
rhuang4 <ruuka.huang4@gilead.com>
2022-06-29 12:40:06 -0400

    Update to new axios API, disable proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,13 +115,14 @@ export default {
 
   proxy: {
     // Simple proxy
-    '/api': 'http://localhost:8000',
+    // '/api': 'http://localhost:8000',
+    // '/api': 'http://sjggpappprdn09:8000',
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-     baseURL: '/api/v1/',
+    baseURL: 'http://sjggpappprdn09:8000/api/v1/',
     // baseURL: 'https://bioinformatics.gilead.com/api/v1/',
   },
 
