1f90141e2d1b9651f672f5b00d4256e6083735c8
Boratechlife <boratechlife@gmail.com>
2022-12-08 15:58:57 +0300

    fix table and logo

--- components/home_components/TableComponent.vue
+++ components/home_components/TableComponent.vue
@@ -20,7 +20,7 @@
             </div>
           </td>
           <td class="border px-2 py-3">Phase</td>
-          <td class="border px-2 py-3">TA</td>
+
           <td class="border px-2 py-3">Product</td>
           <td class="border px-2 py-3">Samples</td>
           <td class="border px-2 py-3"></td>
@@ -33,45 +33,57 @@
           class="text-[12px]"
         >
           <td class="w-1/4 border bg-[#644ded14] px-2 py-3">
-            <div class="flex gap-2 utems-center">
+            <div
+              class="flex gap-2 hover:text-primary cursor-pointer items-center"
+              :class="äctiveId === item.study_id ? 'text-primary' : ''"
+              @click="äctiveId = item.study_id"
+            >
               <span>{{ item.study_id }}</span>
             </div>
           </td>
           <td class="border px-2 py-3">{{ item.phase }}</td>
-          <td class="border px-2 py-3">{{ item.TA }}</td>
+
           <td class="border px-2 py-3">{{ item.product }}</td>
           <td class="border px-2 py-3">{{ item.samples }}</td>
           <td class="border px-2 py-3">
-            <a
-              :href="baseURL + item.id"
-              target="_blank"
-              class="text-[#644DED]"
-              title="A Phase 2, Randomized, Double-blind, Placebo-Controlled Study evaluating the safety and efficacy of Selonsertib, GS-0976, GS-9674, and Combinations in subjects with bridging (F3) Fibrosis or Compensated Cirrhosis (F4) due to Nonalcoholic Steatohepatitis (NASH)"
-              ><svg
-                class="h-[16px]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
+            <div class="flex gap-2 items-center">
+              <a
+                :href="baseURL + item.id"
+                target="_blank"
+                class="text-[#644DED]"
+                title="A Phase 2, Randomized, Double-blind, Placebo-Controlled Study evaluating the safety and efficacy of Selonsertib, GS-0976, GS-9674, and Combinations in subjects with bridging (F3) Fibrosis or Compensated Cirrhosis (F4) due to Nonalcoholic Steatohepatitis (NASH)"
+                ><svg
+                  class="h-[16px]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"
+                  /></svg
+              ></a>
+              <a href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-4 h-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                  />
+                </svg>
+              </a>
+              <button
+                v-if="äctiveId === item.study_id"
+                class="px-2 py-1 text-sm bg-primary rounded text-white"
               >
-                <path
-                  d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"
-                /></svg
-            ></a>
-            <a href="">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-4 h-4"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-                />
-              </svg>
-            </a>
+                Make Plot
+              </button>
+            </div>
           </td>
         </tr>
         <tr>
@@ -152,6 +164,7 @@ export default {
       searchTerm: null,
       perPage: 5,
       currentPage: 1,
+      äctiveId: null,
     }
   },
   computed: {
--- components/layout_components/navBars.vue
+++ components/layout_components/navBars.vue
@@ -18,96 +18,95 @@
                 alt="Gilead"
               />
             </NuxtLink>
-            <NuxtLink to="/">
-              <img
-                class="hidden w-auto h-10 lg:block"
-                src="~/static/cbea-logo.svg"
-                alt="CBEA"
-              />
-            </NuxtLink>
           </div>
-          <div class="hidden sm:block sm:ml-6">
+          <div class="hidden sm:flex justify-between w-full sm:ml-6">
             <div class="flex space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <NuxtLink
                 to="/new-ui"
                 class="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-md"
                 aria-current="page"
-              >New UI</NuxtLink
+                >New UI</NuxtLink
               >
               <NuxtLink
                 to="/custom-plot"
                 class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-              >Bioinformatics Data Hub</NuxtLink
+                >Bioinformatics Data Hub</NuxtLink
               >
               <NuxtLink
                 to="/clinical-bioinformatics"
                 class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-              >Publications</NuxtLink
+                >Publications</NuxtLink
               >
               <a
                 href="http://bioinfo.na.gilead.com/CPD/"
                 target="_blank"
                 class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-              >Common Practice</a
+                >Common Practice</a
               >
               <a
                 href="http://bioinfo.na.gilead.com/tableau/"
                 target="_blank"
                 class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-              >Tableau</a
+                >Tableau</a
               >
             </div>
           </div>
+          <NuxtLink to="/">
+            <img
+              class="hidden w-auto h-10 lg:block"
+              src="~/static/cbea-logo.svg"
+              alt="CBEA"
+            />
+          </NuxtLink>
         </div>
 
-<!--        <div-->
-<!--          class="flex items-center justify-center flex-2 sm:items-stretch sm:justify-start"-->
-<!--        >-->
-<!--          <button-->
-<!--            data-collapse-toggle="mobile-menu-3"-->
-<!--            type="button"-->
-<!--            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"-->
-<!--            aria-controls="mobile-menu-3"-->
-<!--            aria-expanded="false"-->
-<!--          >-->
-<!--            <span class="sr-only">Open main menu</span>-->
-<!--            <svg-->
-<!--              class="w-6 h-6"-->
-<!--              fill="currentColor"-->
-<!--              viewBox="0 0 20 20"-->
-<!--              xmlns="http://www.w3.org/2000/svg"-->
-<!--            >-->
-<!--              <path-->
-<!--                fill-rule="evenodd"-->
-<!--                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"-->
-<!--                clip-rule="evenodd"-->
-<!--              ></path>-->
-<!--            </svg>-->
-<!--            <svg-->
-<!--              class="hidden w-6 h-6"-->
-<!--              fill="currentColor"-->
-<!--              viewBox="0 0 20 20"-->
-<!--              xmlns="http://www.w3.org/2000/svg"-->
-<!--            >-->
-<!--              <path-->
-<!--                fill-rule="evenodd"-->
-<!--                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"-->
-<!--                clip-rule="evenodd"-->
-<!--              ></path>-->
-<!--            </svg>-->
-<!--          </button>-->
-<!--        </div>-->
+        <!--        <div-->
+        <!--          class="flex items-center justify-center flex-2 sm:items-stretch sm:justify-start"-->
+        <!--        >-->
+        <!--          <button-->
+        <!--            data-collapse-toggle="mobile-menu-3"-->
+        <!--            type="button"-->
+        <!--            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"-->
+        <!--            aria-controls="mobile-menu-3"-->
+        <!--            aria-expanded="false"-->
+        <!--          >-->
+        <!--            <span class="sr-only">Open main menu</span>-->
+        <!--            <svg-->
+        <!--              class="w-6 h-6"-->
+        <!--              fill="currentColor"-->
+        <!--              viewBox="0 0 20 20"-->
+        <!--              xmlns="http://www.w3.org/2000/svg"-->
+        <!--            >-->
+        <!--              <path-->
+        <!--                fill-rule="evenodd"-->
+        <!--                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"-->
+        <!--                clip-rule="evenodd"-->
+        <!--              ></path>-->
+        <!--            </svg>-->
+        <!--            <svg-->
+        <!--              class="hidden w-6 h-6"-->
+        <!--              fill="currentColor"-->
+        <!--              viewBox="0 0 20 20"-->
+        <!--              xmlns="http://www.w3.org/2000/svg"-->
+        <!--            >-->
+        <!--              <path-->
+        <!--                fill-rule="evenodd"-->
+        <!--                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"-->
+        <!--                clip-rule="evenodd"-->
+        <!--              ></path>-->
+        <!--            </svg>-->
+        <!--          </button>-->
+        <!--        </div>-->
 
-<!--        <div id="mobile-menu-3" class="absolute right-0 w-48 py-1 mt-60 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">-->
-<!--          <NuxtLink-->
-<!--            to="/new-ui" class="block px-4 py-2 text-sm text-gray-700" aria-current="page" role="menuitem" tabindex="-1" id="user-menu-item-0"-->
-<!--          >New UI</NuxtLink-->
-<!--          >-->
-<!--          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>-->
-<!--          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>-->
-<!--        </div>-->
-
+        <!--        <div id="mobile-menu-3" class="absolute right-0 w-48 py-1 mt-60 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">-->
+        <!--          <NuxtLink-->
+        <!--            to="/new-ui" class="block px-4 py-2 text-sm text-gray-700" aria-current="page" role="menuitem" tabindex="-1" id="user-menu-item-0"-->
+        <!--          >New UI</NuxtLink-->
+        <!--          >-->
+        <!--          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>-->
+        <!--          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>-->
+        <!--        </div>-->
       </div>
     </div>
 
@@ -119,29 +118,29 @@
           to="/new-ui"
           class="px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-md"
           aria-current="page"
-        >New UI</NuxtLink
+          >New UI</NuxtLink
         >
         <NuxtLink
           to="/custom-plot"
           class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-        >Bioinformatics Data Hub</NuxtLink
+          >Bioinformatics Data Hub</NuxtLink
         >
         <NuxtLink
           to="/clinical-bioinformatics"
           class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-        >Publications</NuxtLink
+          >Publications</NuxtLink
         >
         <a
           href="http://bioinfo.na.gilead.com/tableau/#/knowledge-base"
           target="_blank"
           class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-        >Common Practice</a
+          >Common Practice</a
         >
         <a
           href="http://bioinfo.na.gilead.com/tableau/"
           target="_blank"
           class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-        >Tableau</a
+          >Tableau</a
         >
       </div>
     </div>
